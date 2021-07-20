004f566767c98d428011f776e6005e52c55b1efd
Kirill <spitefulshaman@gmail.com>
2021-07-20 11:08:02 +0300

    Init project

---
+++ tapTest/tapTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  tapTest
+//
+//  Created by Kirill Afanasiev on 20.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//        self.view.backgroundColor = UIColor.red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
